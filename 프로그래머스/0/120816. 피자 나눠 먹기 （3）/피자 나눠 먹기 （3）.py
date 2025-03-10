2718c3e6c3d295a7969c77a613d287567cc8fab7
Seo Seong Jun <167270556+SSjunn@users.noreply.github.com>
2025-03-10 21:37:34 +0900

    [level 0] Title: 피자 나눠 먹기 (3), Time: 0.00 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/0/120816. 피자 나눠 먹기 （3）/피자 나눠 먹기 （3）.py
+++ 프로그래머스/0/120816. 피자 나눠 먹기 （3）/피자 나눠 먹기 （3）.py
@@ -2,5 +2,5 @@ def solution(slice, n):
     answer = n // slice # n명을 slice로 나눈 몫을 answer에 저장 
     
     if n%slice!=0: #정확히 나누어지지 않아
-        answer +=1
+        answer +=1 # 한판 추가
     return answer
