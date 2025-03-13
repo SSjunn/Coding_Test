6c6567e51f2ebabda99ef8aba0e9f8cb57a8b45a
Seo Seong Jun <167270556+SSjunn@users.noreply.github.com>
2025-03-13 16:47:07 +0900

    Update 점의 위치 구하기.py

--- 프로그래머스/0/120841. 점의 위치 구하기/점의 위치 구하기.py
+++ 프로그래머스/0/120841. 점의 위치 구하기/점의 위치 구하기.py
@@ -13,6 +13,6 @@ def solution(dot):
 '''
 # 이게 더 빠른 방법이야.
 def solution(dot):
-    quad = [(3,2),(4,1)]
-    return quad[dot[0] > 0][dot[1] > 0]
+    quad = [(3,2),(4,1)]  # 사분면을 저장한 리스트
+    return quad[dot[0] > 0][dot[1] > 0] # 조건에 따라 사분면 번호 선택!!
 '''
