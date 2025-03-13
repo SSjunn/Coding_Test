1a4d8d7767f05d0b7044b134bcf3064cdd714a79
Seo Seong Jun <167270556+SSjunn@users.noreply.github.com>
2025-03-13 15:47:06 +0900

    Update 점의 위치 구하기.py

--- 프로그래머스/0/120841. 점의 위치 구하기/점의 위치 구하기.py
+++ 프로그래머스/0/120841. 점의 위치 구하기/점의 위치 구하기.py
@@ -9,3 +9,10 @@ def solution(dot):
         return 3
     if x > 0 and y<0:
         return 4
+
+'''
+# 이게 더 빠른 방법이야.
+def solution(dot):
+    quad = [(3,2),(4,1)]
+    return quad[dot[0] > 0][dot[1] > 0]
+'''
