8ced1cc5337f6db9b4fc67461fcff687456216b9
Seo Seong Jun <167270556+SSjunn@users.noreply.github.com>
2025-07-24 14:42:09 +0900

    Update 편지.py

--- 프로그래머스/0/120898. 편지/편지.py
+++ 프로그래머스/0/120898. 편지/편지.py
@@ -2,5 +2,6 @@ def solution(message):
     return len(message)*2
 '''
     이게 최적화인가?
+    실행 속도가 더 빠르지는 않지만 가독성이나 디버깅 측명에서 def 로 정의한 함수가 더 좋을때가 있다.
     result = lambda message: len(message) * 2
 '''
