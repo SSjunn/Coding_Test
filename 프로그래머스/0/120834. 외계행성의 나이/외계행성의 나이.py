97e1f11eda78db6f3a9fcea262dfc1c48c3a5dd9
Seo Seong Jun <167270556+SSjunn@users.noreply.github.com>
2025-07-24 15:28:10 +0900

    Update 외계행성의 나이.py

--- 프로그래머스/0/120834. 외계행성의 나이/외계행성의 나이.py
+++ 프로그래머스/0/120834. 외계행성의 나이/외계행성의 나이.py
@@ -3,3 +3,10 @@ def solution(age):
     for i in str(age):
         answer += chr(int(i) + 97)
     return answer
+
+'''
+    아스키코드 'a' = 97
+    a=0, b=1, c=2... 이렇다면 
+    age = 23이라면 str(age) -> '2', '3' 꺼냄
+    char(int(i) + 97) -> char(2+97) = c 가 출력 그러면 예시의 2를 했는데 c가 나오는게 맞다.
+'''
