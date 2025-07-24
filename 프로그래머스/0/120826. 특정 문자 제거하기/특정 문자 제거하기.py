2c6d39c67d60ecfcc9437c69400d8510be21f37b
Seo Seong Jun <167270556+SSjunn@users.noreply.github.com>
2025-07-24 15:51:11 +0900

    Update 특정 문자 제거하기.py

--- 프로그래머스/0/120826. 특정 문자 제거하기/특정 문자 제거하기.py
+++ 프로그래머스/0/120826. 특정 문자 제거하기/특정 문자 제거하기.py
@@ -1,3 +1,13 @@
 def solution(my_string, letter):
     return my_string.replace(letter, "")
 # 문자열.replace(바꿀문자, 새로운 문자)
+
+'''
+    def solution(my_string, letter):
+        result = ""
+        for ch in my_string:
+            if ch != letter:
+                result +=ch
+        return result
+    print(solution("banana", "a")) # "bnn" 이 출력
+'''
