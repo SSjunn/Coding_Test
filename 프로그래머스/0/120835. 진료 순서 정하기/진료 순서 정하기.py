1f7d2553f6eb9d5c4fba7e884aef7fb59a944cf8
Seo Seong Jun <167270556+SSjunn@users.noreply.github.com>
2025-08-02 18:58:53 +0900

    Update 진료 순서 정하기.py

--- 프로그래머스/0/120835. 진료 순서 정하기/진료 순서 정하기.py
+++ 프로그래머스/0/120835. 진료 순서 정하기/진료 순서 정하기.py
@@ -1,11 +1,28 @@
 def solution(emergency):
+    # 1. 내림차순 정렬해서 순위 정보 만들기
+    rank = {num: i+1 for i, num in enumerate(sorted(emergency, reverse=True))}
+    
+    # 2. 원래 배열 순서대로 순위 매기기
+    answer = [rank[num] for num in emergency]
+    return answer
+
+# 1단계 : 내림차순 정렬
+# sorted(emergency, reverse=True)
+
+# 정렬된 리스트에 순서 부여
+# for i, num in enumerate([])
+
+다른 방법
+def solution(emergency):
     n = len(emergency)
-    rank = [1] * n  # 모든 사람의 초기 등수는 1등으로 설정
+    # 모든 사람의 초기 등수는 1등으로 설정
+    rank = [1] * n
 
     # 자기보다 높은 숫자가 몇개인지
     for i in range(n):
         for j in range(n):
-            if emergency[i] < emergency[j]:
-                rank[i] += 1
+            if emergency[i] < emergnecy[j]:
+                ranke[i]+= 1
 
-    return rank
+reutnr rank
+            
