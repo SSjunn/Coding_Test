4e804fca40f0451391c7e62ff6a5d6ec0a846059
Seo Seong Jun <167270556+SSjunn@users.noreply.github.com>
2025-11-17 14:03:23 +0900

    [level 0] Title: 수열과 구간 쿼리 3, Time: 0.06 ms, Memory: 87.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/181924. 수열과 구간 쿼리 3/수열과 구간 쿼리 3.java
@@ -0,0 +1,22 @@
+/*
+    temp = a
+    a = b
+    b = temp
+    이거 사용해서 바꾸기
+
+*/
+
+class Solution {
+    public int[] solution(int[] arr, int[][] queries) {
+        for (int i = 0; i < queries.length; i++) {
+            int a = queries[i][0];
+            int b = queries[i][1];
+
+            int temp = arr[a];
+            arr[a] = arr[b];
+            arr[b] = temp;
+        }
+        return arr;
+    }
+}
+
