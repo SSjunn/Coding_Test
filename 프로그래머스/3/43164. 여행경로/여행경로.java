f0b64e992eb488cbf3e75f9f5bef6003b70e42aa
Seo Seong Jun <167270556+SSjunn@users.noreply.github.com>
2026-02-19 19:01:40 +0900

    [level 3] Title: 여행경로, Time: 0.73 ms, Memory: 73.3 MB -BaekjoonHub

--- 프로그래머스/3/43164. 여행경로/여행경로.java
+++ 프로그래머스/3/43164. 여행경로/여행경로.java
@@ -1,9 +1,9 @@
 import java.util.*;
 
 class Solution {
-    private boolean[] used; // 티켓 체크
-    private String[] answer; // 최단경로 저장
-    private boolean found = false; // 실패면 계속 찾음
+    private boolean[] used;
+    private String[] answer;
+    private boolean found = false;
     
     public String[] solution(String[][] tickets) {
         Arrays.sort(tickets, (a, b) -> a[1].compareTo(b[1])); // 도착지 알파벳 순 정렬
@@ -17,7 +17,6 @@ class Solution {
         return answer;
     }
     
-    // 내 공항, 전체 티켓 목록, 사용한 티켓수
     private void dfs(String current, String[][] tickets, int depth) {
         if (found) return;
         
@@ -32,7 +31,7 @@ class Solution {
                 used[i] = true;
                 answer[depth + 1] = tickets[i][1];
                 dfs(tickets[i][1], tickets, depth + 1);
-                if (found) return; // 정답 찾으면 끝
+                if (found) return; // 정답 찾으면 즉시 종료
                 used[i] = false;
             }
         }
