94a2b1466f2e3e5753b3477d448a03bb1a73cfae
Seo Seong Jun <167270556+SSjunn@users.noreply.github.com>
2026-06-03 12:48:14 +0900

    [level 0] Title: 문자열 출력하기, Time: 88.68 ms, Memory: 63.2 MB -BaekjoonHub

--- 프로그래머스/0/181952. 문자열 출력하기/문자열 출력하기.java
+++ 프로그래머스/0/181952. 문자열 출력하기/문자열 출력하기.java
@@ -1,9 +1,18 @@
 import java.util.Scanner;
+/*
+Scanner 입력 메서드 종류
 
+nextLine() -> 한 줄 전체를 읽음(공백 포함)
+next() -> 공백 전까지 단어 하나 읽기
+nextInt() -> 정수 읽기
+nextLong() -> 큰 정수 읽기
+nextDouble() -> 실수 읽기
+nextBoolean() -> true/false 읽기
+*/
 public class Solution {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String a = sc.nextLine();
-        System.out.println(a);
+        String str = sc.nextLine();
+        System.out.println(str);
     }
 }
