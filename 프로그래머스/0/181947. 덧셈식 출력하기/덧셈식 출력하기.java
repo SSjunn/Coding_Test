510a4c8d54625c27c9b13185fbd3853cc945f4c1
Seo Seong Jun <167270556+SSjunn@users.noreply.github.com>
2026-06-04 13:17:19 +0900

    [level 0] Title: 덧셈식 출력하기, Time: 92.58 ms, Memory: 60.5 MB -BaekjoonHub

--- 프로그래머스/0/181947. 덧셈식 출력하기/덧셈식 출력하기.java
+++ 프로그래머스/0/181947. 덧셈식 출력하기/덧셈식 출력하기.java
@@ -1,11 +1,14 @@
-import java.util.Scanner;
-
+import java.util.*;
+/*
+정수 -> nextInt()로 값 받으면 됌
+*/
 public class Solution {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
         int b = sc.nextInt();
-
-        System.out.println(a+" + "+b +" = "+(a+b));
+        
+        System.out.print(a + " + " + b + " = " + (a+b));
+        
     }
 }
