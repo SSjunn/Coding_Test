f86e249ba6d0a672db195cba03387417521b3f79
Seo Seong Jun <167270556+SSjunn@users.noreply.github.com>
2026-06-15 10:37:45 +0900

    Update 문자열 붙여서 출력하기.java

--- 프로그래머스/0/181946. 문자열 붙여서 출력하기/문자열 붙여서 출력하기.java
+++ 프로그래머스/0/181946. 문자열 붙여서 출력하기/문자열 붙여서 출력하기.java
@@ -1,12 +1,25 @@
 import java.util.Scanner;
 
-public class Solution {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String a = sc.next();
-        String b = sc.next();
+// public class Solution {
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         String a = sc.next();
+//         String b = sc.next();
        
-        String ab = a+b;
-        System.out.println(ab);
+//         String ab = a+b;
+//         System.out.println(ab);
+//     }
+// }
+
+
+
+
+public class Solution {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        
+        String str12 = sc.nextLine();
+        String str12_re = str12.replace(" ","");
+        System.out.println(str12_re);
     }
 }
