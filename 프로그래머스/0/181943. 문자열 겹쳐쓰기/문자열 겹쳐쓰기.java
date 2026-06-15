0e33a3470df497f8d89112be20c7b250146007b6
Seo Seong Jun <167270556+SSjunn@users.noreply.github.com>
2026-06-15 12:00:51 +0900

    [level 0] Title: 문자열 겹쳐쓰기, Time: 4.89 ms, Memory: 79.1 MB -BaekjoonHub

--- 프로그래머스/0/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.java
+++ 프로그래머스/0/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.java
@@ -5,6 +5,7 @@
     아닌가?
     my_sting - over 해주고 index 변경 함수 쓰면 되는거 같은데
 */
+/*
 class Solution {
     public String solution(String my_string, String overwrite_string, int s) {
         String changed = "";
@@ -15,3 +16,40 @@ class Solution {
         return changed;
     }
 }
+*/
+
+public class Solution{
+    public String solution(String my_string, String overwrite_string, int s){
+        
+        int ml = my_string.length();
+        int ol = overwrite_string.length();
+        
+        
+        String answer = my_string.substring(0,s) + overwrite_string + my_string.substring(s+ol);
+        
+        System.out.println(answer);
+        return answer;
+    }
+}
+
+/*
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        String my_string = sc.nextLine();
+        String overwrite_string = sc.nextLine();
+        
+        System.out.print("시작할 위치(숫자) 입력: ");
+        int s = sc.nextInt();
+        
+        int ol = overwrite_string.length();
+        
+        String answer = my_string.substring(0, s) + overwrite_string + my_string.substring(s + ol);
+        
+        System.out.println("최종 결과: " + answer);
+    }
+}
+*/
