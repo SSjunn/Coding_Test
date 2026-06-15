8b2bcf804e5ec6b5a5da55148cbc2e850cfe71b7
Seo Seong Jun <167270556+SSjunn@users.noreply.github.com>
2026-06-15 11:05:38 +0900

    [level 0] Title: 문자열 돌리기, Time: 88.39 ms, Memory: 68.9 MB -BaekjoonHub

--- 프로그래머스/0/181945. 문자열 돌리기/문자열 돌리기.java
+++ 프로그래머스/0/181945. 문자열 돌리기/문자열 돌리기.java
@@ -1,10 +1,10 @@
 import java.util.Scanner;
-
 /*
     str 문자열
     배열에 하나씩 담아서
     for 문 반복으로 index 하나씩 출력
 */
+/*
 public class Solution {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -13,5 +13,16 @@ public class Solution {
         for (int i =0; i<aArr.length; i++){
             System.out.println(aArr[i]);
         }
+    }
+}
+*/
+public class Solution {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        String[] answer = str.split("");
+        for (int i = 0; i < answer.length; i++){
+            System.out.println(answer[i]);
         }
     }
+}
