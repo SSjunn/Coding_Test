2496f8861cc2edc80b6e45a64ba3d365b84cc982
Seo Seong Jun <167270556+SSjunn@users.noreply.github.com>
2026-06-22 11:39:08 +0900

    [level 0] Title: 문자열 섞기, Time: 0.05 ms, Memory: 81.4 MB -BaekjoonHub

--- 프로그래머스/0/181942. 문자열 섞기/문자열 섞기.java
+++ 프로그래머스/0/181942. 문자열 섞기/문자열 섞기.java
@@ -1,3 +1,4 @@
+/*
 class Solution {
     public String solution(String str1, String str2) {
         String result ="";
@@ -9,4 +10,26 @@ class Solution {
         return result;
     }
     
+}
+*/
+
+// String result = "" 방식은 새로운 String 객체를 계속해서 생성을 하기 때문에 O(n²) 시간 복잡도가 나오게 된다. 따라서 StingBuilder 를 사용하여 시간을 O(n) 시간으로 줄여서 사용을 해준다.
+
+class Solution {
+    public String solution(String str1, String str2) {
+        StringBuilder sb = new StringBuilder();
+        int i = 0; int j = 0;
+        
+        while (i < str1.length() || j < str2.length()) {
+            if (i < str1.length()) {
+                sb.append(str1.charAt(i));
+                i++;
+            }
+            if (j < str2.length()) {
+                sb.append(str2.charAt(j));
+                j++;
+            }
+        }
+        return sb.toString();
+    }
 }
