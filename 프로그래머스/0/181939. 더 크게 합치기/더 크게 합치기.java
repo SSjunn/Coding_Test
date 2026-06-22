308c14b6acadb15dce1e4ac3d4a28c506cf0a260
Seo Seong Jun <167270556+SSjunn@users.noreply.github.com>
2026-06-22 13:34:58 +0900

    [level 0] Title: 더 크게 합치기, Time: 0.88 ms, Memory: 79.4 MB -BaekjoonHub

--- 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.java
+++ 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.java
@@ -1,3 +1,4 @@
+/*
 class Solution {
     public int solution(int a, int b) {
         int answer = 0;
@@ -5,5 +6,20 @@ class Solution {
         int result2 = Integer.parseInt(Integer.toString(b) + Integer.toString(a));
         answer = (result2 <= result1) ? result1 : result2;
         return answer;
+    }
+}
+*/
+
+class Solution{
+    public int solution(int a, int b){
+        String case1 = (String.valueOf(a) + String.valueOf(b));
+        String case2 = (String.valueOf(b) + String.valueOf(a));
+        
+        int case1_i = Integer.valueOf(case1);
+        int case2_i = Integer.valueOf(case2);
+        
+        int result = (case1_i>case2_i) ? case1_i : case2_i;
+        
+        return result;
     }
 }
