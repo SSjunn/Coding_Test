43465c9e422e32e230aba4a221c6417208658b94
Seo Seong Jun <167270556+SSjunn@users.noreply.github.com>
2026-06-22 11:59:47 +0900

    [level 0] Title: 문자열 곱하기, Time: 0.05 ms, Memory: 74.6 MB -BaekjoonHub

--- 프로그래머스/0/181940. 문자열 곱하기/문자열 곱하기.java
+++ 프로그래머스/0/181940. 문자열 곱하기/문자열 곱하기.java
@@ -1,3 +1,4 @@
+/*
 class Solution {
     public String solution(String my_string, int k) {
         String answer = "";
@@ -5,5 +6,16 @@ class Solution {
             answer += my_string;
         }
         return answer;
+    }
+}
+*/
+class Solution{
+    public String solution (String my_string, int k){
+        StringBuilder sb = new StringBuilder();
+        
+        for (int i = 0; i<k; i++){
+            sb.append(my_string);
+        }
+        return sb.toString();
     }
 }
